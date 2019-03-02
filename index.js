730f18014bf8650d67abc230489b4b40bb9cf83b
Erin Bassity <erin.bassity@cognizant.com>
2019-03-02 11:44:10 -0700

    Set up passport for oauth

--- index.js
+++ index.js
@@ -4,16 +4,25 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy
 const kays = require('./config/keys')
 const app = express()
 
-passport.use(new GoogleStrategy({
-  clientID: keys.googleClientID,
-  clientSecret: keys.googleClientSecret
-}))
+passport.use(
+  new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+  },
+    accessToken => {
+      console.log(accessToken)
+    }
+  )
+)
 
-app.get('/', (req, res) => {
-  res.send({ key: 'value' })
-})
-
-
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+    // scope: google has a list of all the different items we can ask for access to
+  })
+)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
