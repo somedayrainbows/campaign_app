351384fc7fe71aa1ee15894a18b8a547c09b47f1
Erin Bassity <erin.bassity@cognizant.com>
2019-03-02 11:37:20 -0700

    Add keys

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
-
+const kays = require('./config/keys')
 const app = express()
 
-passport.use(new GoogleStrategy())
+passport.use(new GoogleStrategy({
+  clientID: keys.googleClientID,
+  clientSecret: keys.googleClientSecret
+}))
 
 app.get('/', (req, res) => {
   res.send({ key: 'value' })
