20ebfb43a2088420e1a3e8884d4f1b026fe669bb
Erin Bassity <erin.bassity@cognizant.com>
2019-03-02 15:45:47 -0700

    Refactor routes and services

--- index.js
+++ index.js
@@ -1,33 +1,9 @@
 const express = require('express')
-const passport = require('passport')
-const GoogleStrategy = require('passport-google-oauth20').Strategy
-const keys = require('./config/keys')
+require('./services/passport')
+
 const app = express()
 
-passport.use(
-  new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-  },
-    accessToken => {
-      console.log(accessToken)
-    }
-  )
-)
-
-app.get(
-  '/auth/google',
-  passport.authenticate('google', {
-    scope: ['profile', 'email']
-    // scope: google has a list of all the different items we can ask for access to
-  })
-)
-
-app.get(
-  '/auth/google/callback',
-  
-)
+require('./routes/authRoutes')(app)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
---
+++ routes/authRoutes.js
@@ -0,0 +1,13 @@
+const passport = require('passport')
+
+module.exports = (app) => {
+  app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+      scope: ['profile', 'email']
+      // scope: google has a list of all the different items we can ask for access to
+    })
+  )
+
+  app.get('/auth/google/callback', passport.authenticate('google'))
+}
