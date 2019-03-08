8122187f46473fe854bd37318e0083d5ca80efc2
Erin Bassity <erin.bassity@cognizant.com>
2019-03-07 22:27:47 -0700

    Update redirect URI

--- config/prod.js
+++ config/prod.js
@@ -3,5 +3,5 @@ module.exports = {
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
-  googleRedirectURI: 'https://thawing-sea-14349.herokuapp.com/'
+  googleRedirectURI: 'https://thawing-sea-14349.herokuapp.com'
 }
