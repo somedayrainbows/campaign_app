82064bbffd1f17c6b71c4c4df14b204638ae3dc8
Erin Bassity <erin.bassity@cognizant.com>
2019-03-08 10:38:39 -0700

    update db connection

--- config/prod.js
+++ config/prod.js
@@ -2,6 +2,5 @@ module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
-  cookieKey: process.env.COOKIE_KEY,
-  googleRedirectURI: process.env.GOOGLE_REDIRECT_URI
+  cookieKey: process.env.COOKIE_KEY
 }
