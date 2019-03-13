2c1455b65690cf6aecc0aaa23ba7ce6dea87581f
Erin Bassity <erin.bassity@cognizant.com>
2019-03-13 12:26:43 -0600

    Add google auth message

--- client/src/App.js
+++ client/src/App.js
@@ -8,18 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hello! Welcome.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <h2>
+        <a href="/auth/google">Log in with your Google account</a>
+        </h2>
       </div>
     );
   }
