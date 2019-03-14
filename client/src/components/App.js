94e213889d892fb2e378ee780b5b2f365aaac87e
Erin Bassity <erin.bassity@cognizant.com>
2019-03-14 13:37:54 -0600

    Update route configuration

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,14 +2,20 @@ import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 
 const Header = () => <h2>Header</h2>
+const Landing = () => <h2>Landing</h2>
 const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>SurveyNew</h2>
-const Landing = () => <h2>Landing</h2>
 
 const App = () => {
   return (
     <div>
-      Hello!
+      <BrowserRouter>
+        <div>
+          <Route path="/" component={Header} />
+          <Route exact path="/" component={Landing} />
+          <Route path="/surveys" component={Dashboard} />
+        </div>
+      </BrowserRouter>
     </div>
   )
 }
