9575122c914c36fe8d4a1c7d523adef11af79975
Erin Bassity <erin.bassity@cognizant.com>
2019-03-19 13:55:12 -0600

    Create Landing component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 import Header from './Header'
+import Landing from './Landing'
 import { connect } from 'react-redux'
 import * as actions from '../actions'
 
-const Landing = () => <h2>Landing</h2>
 const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>SurveyNew</h2>
 
---
+++ client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Landing = () => {
+  return (
+    <div>
+      <h1>
+        Welcome!
+      </h1>
+      Send surveys with ease.
+    </div>
+  )
+}
+
+export default Landing;
