3be86d9582ab354ab1e6c0c7320e7760e36b87d8
Erin Bassity <erin.bassity@cognizant.com>
2019-03-21 15:19:33 -0600

    Add payments jsx

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -11,7 +11,10 @@ class Header extends Component {
       case false:
         return <li><a href="/auth/google">Login with Google</a></li>
       default:
-        return <li><a href="/api/logout">Log out</a></li>
+        return [
+          <li><Payments/></li>,
+          <li><a href="/api/logout">Log out</a></li>
+        ]
     }
   }
 
