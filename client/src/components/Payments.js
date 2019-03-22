6d3231339cd3d0811797d6b7bc12336eacc8fce5
Erin Bassity <erin.bassity@cognizant.com>
2019-03-22 10:11:09 -0600

    Add name and description to payment flow

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -5,6 +5,8 @@ class Payments extends Component {
   render() {
     return (
       <StripeCheckout
+        name="Campaign App"
+        description="$5 for five survey credits"
         amount={500}
         token={token => console.log(token)}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
