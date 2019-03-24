3d8c95fa4f6e9151e43c95f1944e9ef5ae251827
Erin Bassity <erin.bassity@cognizant.com>
2019-03-23 18:15:38 -0600

    Add stripe route

---
+++ routes/billingRoutes.js
@@ -0,0 +1,17 @@
+const keys = require('../config/keys')
+const stripe = require('stripe')(keys.stripeSecretKey)
+
+module.exports = app => {
+  app.post('/api/stripe', async (req, res) => {
+    const charge = await stripe.charges.create({
+      amount: 500,
+      currency: 'usd',
+      description: '$5 for Campaign App credits',
+      source: req.body.id
+    })
+    req.user.credits += 5
+    const user = await req.user.save()
+
+    res.send(user)
+  })
+}
